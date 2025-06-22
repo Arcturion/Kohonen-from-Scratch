1d5906153a2a104802330ce6586ffc00f99ac22f
Arcturion <dimascours@gmail.com>
2025-06-22 16:01:55 +0700

    Create __init__.py

---
+++ kohonen/__init__.py
@@ -0,0 +1,10 @@
+"""
+Kohonen Triton - A PyTorch-based Self-Organizing Map (SOM) library accelerated with Triton.
+"""
+from .som import SOM
+
+__version__ = "0.1.0" # Initial version
+
+__all__ = [
+    "SOM"
+]
